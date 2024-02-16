e9ba3dbbca2fcfd9040c474d18abb778397e1448
parthNariya <parthknariya1919@gmail.com>
2024-02-16 17:28:46 +0530

    server actions

---
+++ actions/createBoard/types.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+import { Board } from "@prisma/client";
+import { ActionState } from "@/lib/createSafeAction";
+import { CreateBoardSchema } from "./schema";
+export type InputType = z.infer<typeof CreateBoardSchema>;
+export type ReturnType = ActionState<InputType, Board>;
--- app/(product)/(dashboard)/org/[orgId]/page.tsx
+++ app/(product)/(dashboard)/org/[orgId]/page.tsx
@@ -1,3 +1,19 @@
+import { createBoard } from "@/actions/createBoard";
+import { Button } from "@/components/ui/button";
+
 export default function OrgPage() {
-  return <div>org page</div>;
+  return (
+    <div>
+      <form action={createBoard}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter board title"
+          className="border-black border p-1"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  );
 }
