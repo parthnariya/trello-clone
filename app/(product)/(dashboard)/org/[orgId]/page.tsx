83dcbdeb61d5b965ef4d9d07767b5b47559edb0f
parthNariya <parthknariya1919@gmail.com>
2024-02-21 18:20:34 +0530

    listing of boards

--- app/(product)/(dashboard)/org/[orgId]/page.tsx
+++ app/(product)/(dashboard)/org/[orgId]/page.tsx
@@ -1,6 +1,7 @@
 import { Separator } from "@/components/ui/separator";
 import { Info } from "./_components/Info";
 import { BoardList } from "./_components/BoardList";
+import { Suspense } from "react";
 
 export default function OrgPage() {
   return (
@@ -8,7 +9,9 @@ export default function OrgPage() {
       <Info />
       <Separator className="my-4" />
       <div className="px-2 md:px-4">
-        <BoardList />
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
       </div>
     </div>
   );
